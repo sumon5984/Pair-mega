52a7e7ca743db47f2db8316f328c97e1abc0f4f6
sumon5984 <sbot7603@gmail.com>
2025-09-12 14:52:43 +0530

    Update mega.js

--- utils/mega.js
+++ utils/mega.js
@@ -3,7 +3,7 @@ const mega = require("megajs");
 
 const auth = {
     email: 'sbot7603@gmail.com',
-    password: 'sumon@983655',
+    password: 'sumon@2008@9836',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
